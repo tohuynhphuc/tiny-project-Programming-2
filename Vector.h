9246eab122c31e47546cad79403858188c134a97
MunDepZai <DELL@MacBook-Pro.local>
2025-05-23 20:25:37 +0700

    Fixed vector.h

--- Vector.h
+++ Vector.h
@@ -23,6 +23,11 @@ class Vector {
 
     // Unary Operator
     Vector operator-() const;  // DOES NOT change original vector
+    Vector& Vector::operator++ ();  // DOES change original vector
+    Vector Vector::operator++(int);   // DOES change original vector, but return old copy
+    Vector& Vector::operator--();   // DOES change original vector
+    Vector Vector::operator--(int);   // DOES change original vector, but return old copy
+
 
     // Binary Operator
     // Vector ... const -> return new object
