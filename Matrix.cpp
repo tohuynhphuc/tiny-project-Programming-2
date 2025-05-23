5b832cacdca121165476787e2b4da4a925ac6aff
Quanninh <153052929+Quanninh@users.noreply.github.com>
2025-05-23 09:39:17 +0000

    new_updata

--- Matrix.cpp
+++ Matrix.cpp
@@ -4,3 +4,50 @@
 #include <stdexcept>
 
 using namespace std;
+
+// Allocate memory before copying the entries of the original matrix into the new matrix
+void Matrix::allocateMemory(int rows, int cols){
+    mData = new double*[rows];
+    for (int i = 0; i < rows; i++){
+        mData[i] = new double[cols];
+    }
+}
+
+Matrix:: Matrix(const Matrix& other){
+    mNumRows = other.mNumRows;
+    mNumCols = other.mNumCols;
+    allocateMemory(mNumRows, mNumCols);
+
+    // Copy to new matrix
+    for (int i = 0; i < mNumRows; i++){
+        for (int j = 0; j < mNumCols; j++){
+            mData[i][j] = other.mData[i][j];
+        }
+    }
+}
+
+Matrix::Matrix(int rows, int cols){
+    if (rows <= 0 || cols <= 0) {
+        throw std::invalid_argument("Matrix dimensions must be positive integers.");
+    }
+    mNumRows = rows;
+    mNumCols = cols;
+
+    allocateMemory(mNumRows, mNumCols);
+
+    // Initialize the matrix with 0
+    for (int i = 0; i < mNumRows; i++){
+        for (int j = 0; j < mNumCols; j++){
+            mData[i][j] = 0.0;
+        }
+    }
+
+}
+
+// Destructor to free the allocated memory
+Matrix::~Matrix(){
+    for (int i = 0; i < mNumRows; i++){
+        delete[] mData[i];
+    }
+    delete[] mData;
+}
--- Matrix.h
+++ Matrix.h
@@ -1,4 +1,19 @@
 #ifndef MATRIX_HEADER
 #define MATRIX_HEADER
+#include "Vector.h"
+class Matrix{
+    private:
+    int mNumRows;
+    int mNumCols;
+    double** mData;
+    void allocateMemory(int rows, int cols);   // To allocate 2D array
+
+    public:
+    // Constructor
+    Matrix(int rows, int cols);  // Zero initial matrix
+    Matrix(const Matrix& other);  // Copy constructor
+    // Destructor
+    ~Matrix();
+};
 
 #endif
