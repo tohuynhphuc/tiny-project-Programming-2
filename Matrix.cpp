b0354cda9996e7a11d7661b2c1a77d3a14a43abe
khanh-teo1604 <10423056@student.vgu.edu.vn>
2025-05-23 20:40:47 +0700

    :)

--- Matrix.cpp
+++ Matrix.cpp
@@ -6,28 +6,35 @@
 using namespace std;
 
 // Allocate memory before copying the entries of the original matrix into the new matrix
-void Matrix::allocateMemory(int rows, int cols){
-    mData = new double*[rows];
-    for (int i = 0; i < rows; i++){
+void Matrix::allocateMemory(int rows, int cols)
+{
+    mData = new double *[rows];
+    for (int i = 0; i < rows; i++)
+    {
         mData[i] = new double[cols];
     }
 }
 
-Matrix:: Matrix(const Matrix& other){
+Matrix::Matrix(const Matrix &other)
+{
     mNumRows = other.mNumRows;
     mNumCols = other.mNumCols;
     allocateMemory(mNumRows, mNumCols);
 
     // Copy to new matrix
-    for (int i = 0; i < mNumRows; i++){
-        for (int j = 0; j < mNumCols; j++){
+    for (int i = 0; i < mNumRows; i++)
+    {
+        for (int j = 0; j < mNumCols; j++)
+        {
             mData[i][j] = other.mData[i][j];
         }
     }
 }
 
-Matrix::Matrix(int rows, int cols){
-    if (rows <= 0 || cols <= 0) {
+Matrix::Matrix(int rows, int cols)
+{
+    if (rows <= 0 || cols <= 0)
+    {
         throw std::invalid_argument("Matrix dimensions must be positive integers.");
     }
     mNumRows = rows;
@@ -36,43 +43,52 @@ Matrix::Matrix(int rows, int cols){
     allocateMemory(mNumRows, mNumCols);
 
     // Initialize the matrix with 0
-    for (int i = 0; i < mNumRows; i++){
-        for (int j = 0; j < mNumCols; j++){
+    for (int i = 0; i < mNumRows; i++)
+    {
+        for (int j = 0; j < mNumCols; j++)
+        {
             mData[i][j] = 0.0;
         }
     }
-
 }
 
 // Destructor to free the allocated memory
-Matrix::~Matrix(){
-    for (int i = 0; i < mNumRows; i++){
+Matrix::~Matrix()
+{
+    for (int i = 0; i < mNumRows; i++)
+    {
         delete[] mData[i];
     }
     delete[] mData;
 }
 
 // Access to the number of rows and columns
-int Matrix::getNumRows() const{
+int Matrix::getNumRows() const
+{
     return mNumRows;
 }
 
-int Matrix::getNumCols() const{
+int Matrix::getNumCols() const
+{
     return mNumCols;
 }
 
-// const operator(): only allow to reand 
-double Matrix::operator()(int i, int j){
-    if (i < 1 || i > rows || j < 1 || j > cols){
+// const operator(): only allow to reand
+double Matrix::operator()(int i, int j) const
+{
+    if (i < 1 || i > mNumRows || j < 1 || j > mNumCols)
+    {
         throw std::invalid_argument("Index is out of range");
     }
-    return mData[i-1][j-1];
+    return mData[i - 1][j - 1];
 }
 
 // non-const operator(): can modify and assign to a matrix
-double& Matrix::operator()(int i, int j){
-    if (i < 1 || i > rows || j < 1 || j > cols){
+double &Matrix::operator()(int i, int j)
+{
+    if (i < 1 || i > mNumRows || j < 1 || j > mNumCols)
+    {
         throw std::invalid_argument("Index is out of range");
     }
-    return mData[i-1][j-1];
+    return mData[i - 1][j - 1];
 }
